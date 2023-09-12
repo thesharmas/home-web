c77a9628d27546e8ddd992726d6ad0f501e14020
Rohit <rnsharma@gmail.com>
2023-09-12 08:32:21 -0700

    remove inti

--- gitinit.sh
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Initial Commit"
-git branch -mv master main 
-git remote add origin https://github.com/thesharmas/home-web.git
-git branch -M main
-git push -u origin main
