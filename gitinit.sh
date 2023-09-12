9352eb68213d4cfb1d1d8fc711d4068b7836f068
Rohit <rnsharma@gmail.com>
2023-09-12 08:30:51 -0700

    Initial Commit

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Initial Commit"
+git branch -mv master main 
+git remote add origin https://github.com/thesharmas/home-web.git
+git branch -M main
+git push -u origin main
